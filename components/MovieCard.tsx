9458be56118e1f3cd3f4278ee5cd81018f6bd138
afrotter <jdengelbrecht97@gmail.com>
2023-04-09 18:57:34 +0200

    Thumbnails added to movielist

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -8,7 +8,9 @@ const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
     return (
-        <div></div>
+        <div className="group bg-zinc-900 col-span relative h-[12vw]">
+            <img src={data.thumbnailUrl} alt="Thumbnail" />
+        </div>
     )
 }
 
