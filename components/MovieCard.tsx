461febb7e4ca0f1e5b862dfe4e300fc7529eff98
afrotter <jdengelbrecht97@gmail.com>
2023-04-09 18:59:12 +0200

    Transition effect added

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -9,7 +9,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
 }) => {
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
-            <img src={data.thumbnailUrl} alt="Thumbnail" />
+            <img className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]" src={data.thumbnailUrl} alt="Thumbnail" />
         </div>
     )
 }
