42564d293e46b1f6c3453d5b7a6b200fae4d110b
afrotter <jdengelbrecht97@gmail.com>
2023-04-14 23:12:44 +0200

    Remove/Add to favorite Ids

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { without } from "lodash";
+import { update, without } from "lodash";
 import prismadb from "@/lib/prismadb"
 import serverAuth from "@/lib/serverAuth";
 
@@ -33,7 +33,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
             return res.status(200).json(user);
         }
-    
+
+        if (req.method = 'DELETE') {
+            const { currentUser } = await serverAuth(req);
+
+            const { movieId } = req.body;
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId,
+                }
+            });
+
+            if (!existingMovie) {
+                throw new Error('Invalid ID');
+            }
+
+            const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+
+            const updatedUser = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favoriteIds: updatedFavoriteIds,
+                }
+            });
+
+            return res.status(200).json(updatedUser);
+        }    
     } catch(error) {
         console.log(error);
         return res.status(400).end
