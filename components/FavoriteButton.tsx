f57d60b0c06d348341fa1e92a0efb5c90d3f706e
afrotter <jdengelbrecht97@gmail.com>
2023-04-18 21:01:03 +0200

    Add to favorites dynamic button added

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useCallback, useMemo } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlinePlus, AiOutlineCheck } from "react-icons/ai";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
@@ -38,9 +38,11 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
         mutateFavorites();
     }, [movieId, isFavorite, currentUser, mutate, mutateFavorites])
 
+    const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
+
     return (
-        <div className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
-            <AiOutlinePlus className="text-white" size={25} />
+        <div onClick={toggleFavorites} className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+            <Icon className="text-white" size={25} />
         </div>
     )
 }
